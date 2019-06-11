3d3bd81f7e798c2e0a997360559abd95df4eb24b
Jvymvn <Jordon.carter@yahoo.com>
2019-06-11 16:23:48 -0400

    Changes to parks app

---
+++ Parks app/index.js
@@ -0,0 +1,71 @@
+'use strict';
+
+const apiKey = ""; /*your API key here*/
+
+const searchURL = 'https://api.nps.gov/api/v1/parks';
+
+
+function formatQueryParams(params) {
+  const queryItems = Object.keys(params)
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+  return queryItems.join('&');
+}
+
+function displayResults(responseJson, maxResults) {
+  // if there are previous results, remove them
+  console.log(responseJson);
+  $('#results-list').empty();
+  // iterate through the articles array, stopping at the max number of results
+  for (let i = 0; i < responseJson.data.length & i<maxResults ; i++){
+    // for each video object in the articles
+    //array, add a list item to the results 
+    //list with the article title, source, author,
+    //description, and image
+    //<h3>${responseJson.articles[i].url}">${responseJson.articles[i].title}</h3>
+    $('#results-list').append(
+      `<li><h3>${responseJson.data[i].fullName}</h3>
+      <p>${responseJson.data[i].description}</p>
+      <a href='${responseJson.data[i].url}'>${responseJson.data[i].url}</a>
+      <p>Directions Info / Address: ${responseJson.data[i].directionsInfo}</p>
+      <p>${responseJson.data[i].weatherInfo}</p>
+      </li>`
+    )};
+  //display the results section  
+  $('#results').removeClass('hidden');
+};
+
+function getParks(query, maxResults=10) {
+  const params = {
+    q: query,
+    language: "en",
+    api_key: apiKey,
+    maxResults,
+  };
+  const queryString = formatQueryParams(params)
+  const url = searchURL + '?' + queryString;
+  console.log(url);
+
+
+  fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(responseJson => displayResults(responseJson, maxResults))
+    .catch(err => {
+      $('#js-error-message').text(`Something went wrong: ${err.message}`);
+    });
+}
+
+function watchForm() {
+  $('form').submit(event => {
+    event.preventDefault();
+    const searchTerm = $('#js-search-term').val();
+    const maxResults = $('#js-max-results').val();
+    getParks(searchTerm, maxResults);
+  });
+}
+
+$(watchForm);
