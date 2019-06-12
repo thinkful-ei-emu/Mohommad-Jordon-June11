a136f99b2c3a1482973817b42335b88051e317e8
mak20th <mak20th@gmail.com>
2019-06-12 02:12:39 -0500

    added multiple state input functionality

--- Parks app/index.js
+++ Parks app/index.js
@@ -1,50 +1,81 @@
+/* eslint-disable indent */
+/* eslint-disable no-undef */
 'use strict';
 
-const apiKey = ""; /*your API key here*/
-
-const searchURL = 'https://api.nps.gov/api/v1/parks';
+const apiKey = 'GenR434Z2n5fdh8jqCCbGRdayEzVEGefkZKJIrKC'; /*your API key here*/
+const searchURL = 'https://developer.nps.gov/api/v1/parks';
 
 
 function formatQueryParams(params) {
-  const queryItems = Object.keys(params)
-    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
-  return queryItems.join('&');
+  console.log(`params are ${params}`);
+  if (params.stateCode === '') {
+    params.stateCode = 'xx';
+    //if empty put a character code that doesnt work so we get the correct error message
+  }
+  console.log(`state code entered is ${params.stateCode}`);
+  let codes = params.stateCode.split(',');
+  console.log(`code array code entered is ${codes}`);
+  let str = 'https://developer.nps.gov/api/v1/parks?stateCode=';
+  for (let i = 0; i < codes.length; i++) {
+    if (i === 0) {
+      str += codes[i];
+    } else {
+      str += `%2C${codes[i]}`;
+    }
+  }
+  str += `&limit=${params.maxResults}&api_key=${apiKey}`;
+  return str;
 }
+  // if (params.stateCode === '') {
+  //   params.stateCode = 'xx';
+  //   //if empty put a character code that doesnt work so we get the correct error message
+  // }
+  // let codes = state.split(',');
+  // const queryItems = Object.keys(params)
+  //   .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`);
+  // return queryItems.join('&');
+//}
 
 function displayResults(responseJson, maxResults) {
   // if there are previous results, remove them
   console.log(responseJson);
-  $('#results-list').empty();
-  // iterate through the articles array, stopping at the max number of results
-  for (let i = 0; i < responseJson.data.length & i<maxResults ; i++){
-    // for each video object in the articles
-    //array, add a list item to the results 
-    //list with the article title, source, author,
-    //description, and image
-    //<h3>${responseJson.articles[i].url}">${responseJson.articles[i].title}</h3>
-    $('#results-list').append(
-      `<li><h3>${responseJson.data[i].fullName}</h3>
-      <p>${responseJson.data[i].description}</p>
-      <a href='${responseJson.data[i].url}'>${responseJson.data[i].url}</a>
-      <p>Directions Info / Address: ${responseJson.data[i].directionsInfo}</p>
-      <p>${responseJson.data[i].weatherInfo}</p>
-      </li>`
-    )};
-  //display the results section  
-  $('#results').removeClass('hidden');
-};
+  $('#results-list').html('');
+  if(responseJson.data.length === 0){
+    $('#results-list').html('<p>Please enter a comma delimited list of 2 character state codes');
+  }
+  else{
+     // iterate through the articles array, stopping at the max number of results
+    for (let i = 0; i < responseJson.data.length & i < maxResults ; i++){
+      // for each video object in the articles
+      //array, add a list item to the results 
+      //list with the article title, source, author,
+      //description, and image
+      //<h3>${responseJson.articles[i].url}">${responseJson.articles[i].title}</h3>
+      $('#results-list').append(
+        `<li><h3>${responseJson.data[i].fullName}</h3>
+        <p>${responseJson.data[i].description}</p>
+        <a href='${responseJson.data[i].url}'>Link</a>
+        <p>Directions Info / Address: ${responseJson.data[i].directionsInfo}</p>
+        <p>${responseJson.data[i].weatherInfo}</p>
+        </li>`
+      );
+    //display the results section  
+      $('#results').removeClass('hidden');
+    }
+  }
+   
+}
 
-function getParks(query, maxResults=10) {
+function getNationalParks(query, maxResults=10) {
   const params = {
-    q: query,
-    language: "en",
-    api_key: apiKey,
+    stateCode: query,
+    //language: "en",
+    apiKey,
     maxResults,
   };
-  const queryString = formatQueryParams(params)
+  const queryString = formatQueryParams(params);
   const url = searchURL + '?' + queryString;
   console.log(url);
-
 
   fetch(url)
     .then(response => {
@@ -61,11 +92,23 @@ function getParks(query, maxResults=10) {
 
 function watchForm() {
   $('form').submit(event => {
+    let searchTerm = $('#js-search-term').val();
+    let maxResults = parseInt($('#js-max-results').val());
+    if (isNaN(maxResults) || maxResults <= 0) {
+      maxResults = 10;
+      console.log('User input was incorrect so we defaulted to 10');
+    }
+    $('#js-search-term').val('');
+    $('#js-max-results').val('');
     event.preventDefault();
-    const searchTerm = $('#js-search-term').val();
-    const maxResults = $('#js-max-results').val();
-    getParks(searchTerm, maxResults);
+    getNationalParks(searchTerm, maxResults);
   });
+  // $('form').submit(event => {
+  //   event.preventDefault();
+  //   const searchTerm = $('#js-search-term').val();
+  //   const maxResults = $('#js-max-results').val();
+  //   getNationalParks(searchTerm, maxResults);
+  // });
 }
 
 $(watchForm);
